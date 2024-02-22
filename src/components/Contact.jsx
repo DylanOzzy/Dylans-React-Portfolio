46e37d0fda7d38806680350775768a14eddea9a8
Dylan Osborn <dylanosborn123@hotmail.com>
2024-02-22 13:31:11 -0600

    Added content to footer, and added a title component for easy navigation.

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import Title from './Title'
 
 function Contact() {
     return (
@@ -9,6 +10,7 @@ function Contact() {
                 method="POST"
                 className="flex flex-col w-full md:w-7/12"
                 >
+                    <Title>Contact</Title>
                     <input 
                         type="text"
                         name="name"
@@ -20,11 +22,6 @@ function Contact() {
                         name="email"
                         placeholder="Email"
                         className="my-2 p-2 bg-transparent border-2 rounded-md focus:outline-none"
-                    />
-                    <input 
-                        type="hidden" 
-                        name="_gotcha" 
-                        style="display:none !important"
                     />
                     <textarea 
                         name="message"
